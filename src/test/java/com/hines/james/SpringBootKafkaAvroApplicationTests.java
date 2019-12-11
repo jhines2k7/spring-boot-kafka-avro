d82dded0755a3e1604a3333ba565a62081f970b2
James Hines <jameshines10@gmail.com>
2019-12-10 20:14:32 -0600

    Initial commit

---
+++ src/test/java/com/hines/james/SpringBootKafkaAvroApplicationTests.java
@@ -0,0 +1,98 @@
+package com.hines.james;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.KafkaMessageListenerContainer;
+import org.springframework.kafka.listener.MessageListener;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.rule.EmbeddedKafkaRule;
+import org.springframework.kafka.test.utils.ContainerTestUtils;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertThat;
+import static org.springframework.kafka.test.hamcrest.KafkaMatchers.*;
+
+@RunWith(SpringRunner.class)
+@DirtiesContext
+@SpringBootTest
+@Slf4j
+public class SpringBootKafkaAvroApplicationTests {
+    private static final String TEST_TOPIC_1 = "test-topic-1";
+
+    @ClassRule
+    public static EmbeddedKafkaRule embeddedKafka = new EmbeddedKafkaRule(1, true, TEST_TOPIC_1);
+
+    @Test
+    public void testTemplate() throws Exception {
+        Map<String, Object> consumerProps = KafkaTestUtils.consumerProps("test-consumer-group", "false",
+                embeddedKafka.getEmbeddedKafka());
+
+        DefaultKafkaConsumerFactory<Integer, String> consumerFactory = new DefaultKafkaConsumerFactory<>(consumerProps);
+
+        ContainerProperties containerProperties = new ContainerProperties(TEST_TOPIC_1);
+
+        KafkaMessageListenerContainer<Integer, String> listenerContainer =
+                new KafkaMessageListenerContainer<>(consumerFactory, containerProperties);
+
+        final BlockingQueue<ConsumerRecord<Integer, String>> records = new LinkedBlockingQueue<>();
+
+        listenerContainer.setupMessageListener(new MessageListener<Integer, String>() {
+            @Override
+            public void onMessage(ConsumerRecord<Integer, String> record) {
+                log.info(record.toString());
+                records.add(record);
+            }
+        });
+
+        listenerContainer.setBeanName("templateTests");
+        listenerContainer.start();
+
+        ContainerTestUtils.waitForAssignment(listenerContainer,
+                embeddedKafka.getEmbeddedKafka().getPartitionsPerTopic());
+
+        Map<String, Object> producerProps =
+                KafkaTestUtils.producerProps(embeddedKafka.getEmbeddedKafka());
+
+        ProducerFactory<Integer, String> producerFactory =
+                new DefaultKafkaProducerFactory<>(producerProps);
+
+        KafkaTemplate<Integer, String> template = new KafkaTemplate<>(producerFactory);
+
+        template.setDefaultTopic(TEST_TOPIC_1);
+        template.sendDefault("foo");
+
+        assertThat(records.poll(10, TimeUnit.SECONDS), hasValue("foo"));
+
+        template.sendDefault(0, 2, "bar");
+
+        ConsumerRecord<Integer, String> received = records.poll(10, TimeUnit.SECONDS);
+
+        assertThat(received, hasKey(2));
+        assertThat(received, hasPartition(0));
+        assertThat(received, hasValue("bar"));
+
+        template.send(TEST_TOPIC_1, 0, 2, "baz");
+
+        received = records.poll(10, TimeUnit.SECONDS);
+
+        assertThat(received, hasKey(2));
+        assertThat(received, hasPartition(0));
+        assertThat(received, hasValue("baz"));
+    }
+}
